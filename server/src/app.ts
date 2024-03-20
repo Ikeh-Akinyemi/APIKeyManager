b8d8a001a6ffd3b425309632370ce4bd406ce84e
Ikeh-Akinyemi <61758175+Ikeh-Akinyemi@users.noreply.github.com>
2024-03-20 21:32:26 +0100

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,8 +6,11 @@ import cors from 'cors';
 
 import { sequelize } from "./db/seq/init";
 import * as trpcExpress from "@trpc/server/adapters/express";
-import { appRouter } from "./api/router/_app";
+import { AppRouter, appRouter } from "./api/router/_app";
 import { createContext } from "./api/trpc";
+
+import { applyWSSHandler } from '@trpc/server/adapters/ws';
+import ws from 'ws';
 
 const app = express();
 app.use(cors());
@@ -24,11 +27,22 @@ app.use(
   })
 )
 
-const server = app.listen(configs.Port, () => {
+export const server = app.listen(configs.Port, () => {
   logger.info(`Server started on port ${configs.Port}`);
   sequelize.sync()
-  .then(() => logger.info("DB migration successful"))
-  .catch(reason => logger.error(`Error migrating DB: ${reason}`));
+    .then(() => logger.info("DB migration successful"))
+    .catch(reason => logger.error(`Error migrating DB: ${reason}`));
+});
+
+
+const wss = new ws.Server({ server });
+const handler = applyWSSHandler<AppRouter>({ wss, router: appRouter, createContext });
+
+wss.on('connection', (ws) => {
+  logger.info(`➕➕ Connection (${wss.clients.size})`);
+  ws.once('close', () => {
+    logger.info(`➖➖ Connection (${wss.clients.size})`);
+  });
 });
 
 // Handling graceful shutdown
@@ -38,6 +52,9 @@ const shutdown = async (): Promise<void> => {
   server.close(async () => {
     logger.info("closing db connection...");
     await sequelize.close();
+
+    handler.broadcastReconnectNotification();
+    wss.close();
 
     logger.info("Server shutdown completed");
     process.exit(0);
