5eac1226e782b4e6f2c5e0887978d218c2e23dc3
Ikeh-Akinyemi <61758175+Ikeh-Akinyemi@users.noreply.github.com>
2024-03-21 17:13:52 +0100

    Update _app.ts

--- server/src/api/router/_app.ts
+++ server/src/api/router/_app.ts
@@ -1,11 +1,8 @@
-import { APIKey, sequelize } from "../../db/seq/init";
 import logger from "../../lib/logger/logger";
 import { publicProcedure, router, t } from "../trpc";
 import { apiKeyRouter } from "./apiKey";
 import { authRouter } from "./auth";
 import { userRouter } from "./user";
-import { observable } from '@trpc/server/observable';
-import { Op } from "sequelize";
 
 export const appRouter = router({
   users: userRouter,
@@ -16,40 +13,6 @@ export const appRouter = router({
       status: "success",
       message: "server is healthy"
     }
-  }),
-  keyExpiryNotification: publicProcedure.subscription(async () => {
-    return observable<{ id: number; name: string; websiteUrl: string }>((emit) => {
-      const intervalTime = 200;
-      const checkInterval = 10 * 60000;
-      
-      const timer = setInterval(async () => {
-        try {
-          const now = new Date();
-          const tenMinsLater = new Date(now.getTime() + checkInterval);
-  
-          const expiringKeys = await APIKey.findAll({
-            where: {
-              expiryDate: {
-                [Op.gt]: now,
-                [Op.lt]: tenMinsLater
-              }
-            }
-          });
-  
-          for (const key of expiringKeys) {
-            const { id, name, websiteUrl } = key.get({ plain: true });
-            emit.next({ id, name, websiteUrl });
-          }
-        } catch (error) {
-          logger.error(`Error fetching expiring keys: ${error}`);
-          emit.error(error);
-        }
-      }, intervalTime);
-  
-      return () => {
-        clearInterval(timer);
-      };
-    });
   }),
 });
 
